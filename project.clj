16708b49479715e6f7cadef78f485cbbaaede3c6
Samuel Sutch <sam@sutch.net>
2015-11-18 16:18:40 -0700

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject om-starter "0.1.0-SNAPSHOT"
   :description "My first Om program!"
+  :plugins [[lein-cljsbuild "1.1.1"]]
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.145"]
                  [org.omcljs/om "1.0.0-alpha14"]
@@ -10,4 +11,12 @@
                  [com.cognitect/transit-cljs "0.8.225"]
                  [cljs-http "0.1.30" :exclusions
                   [org.clojure/clojure org.clojure/clojurescript
-                   com.cognitect/transit-cljs]]])
+                   com.cognitect/transit-cljs]]]
+  :source-paths ["src"]
+  :cljsbuild {
+    :builds [
+      {:id "min"
+       :source-paths ["src"]
+       :compiler {:output-to "main.js"
+                  :optimizations :advanced
+                  :pretty-print false}}]})
